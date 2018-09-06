3a76c50908ecf60023b9929019cf027c104e45ce
yangchun <yangchun@stm-macmini-090.local>
2018-09-06 17:30:41 +0800

    make manage img width auto show

--- gif/gif/urls.py
+++ gif/gif/urls.py
@@ -1,14 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from gif_app.views import index, gif
+from gif_app.views import index, gif, manage
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'gif.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
     url(r'^index/', index),
     url(r'^gif/\d+', gif),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^manage/', manage),
 ]
